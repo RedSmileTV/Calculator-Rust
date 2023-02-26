d9c78bb6212cddd9dcc5c5b6297f5e810eb37bab
RedSmile <lelledefeo@gmail.com>
2023-02-26 23:41:42 +0100

    split in multiple functions and made it recursive

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,85 @@
 use std::io;
-use std::thread::sleep;
-use std::time::Duration;
 
 fn main() {
 
-    println!("Rust Calculator 1.2");
+    println!("Rust Calculator 1.4");
     println!();
 
-    let mut num1 = String::new();
-    let mut num2 = String::new();
-    let mut op = String::new();
-
-    eprint!("Enter your first number: ");
-    io::stdin().read_line(&mut num1).expect("failed to read line");
-    println!();
-
-    eprint!("Select an Operator [+, -, *, /]: ");
-    io::stdin().read_line(&mut op).expect("failed to read line");
-    println!();
-
-    eprint!("Enter your second number: ");
-    io::stdin().read_line(&mut num2).expect("failed to read line");
-    println!();
-
-    let num1:f64 = num1.trim().parse().unwrap();
-    let num2:f64 = num2.trim().parse().unwrap();
-    let op = op.trim();
+    let num1 = get_num1();
+    let op = get_op();
+    let num2 = get_num2();
 
     op_checker(op, num1, num2);
 
-    let time = Duration::from_secs(5);
-
-    sleep(time);
-
 }
 
-fn op_checker(op: &str, num1: f64, num2: f64) {
+fn get_num1() -> f64 {
+    let mut num1 = String::new();
+    eprint!("Enter your first number: ");
+    io::stdin().read_line(&mut num1).expect("failed to read line");
+    println!();
+    let num1:f64 = num1.trim().parse().unwrap();
+    return num1;
+}
+
+fn get_op() -> String {
+    let mut op = String::new();
+    eprint!("Select an Operator [+, -, *, /]: ");
+    io::stdin().read_line(&mut op).expect("failed to read line");
+    println!();
+    let op: String = op.trim().to_string();
+    return op;
+}
+
+fn get_num2() -> f64 {
+    let mut num2 = String::new();
+    eprint!("Enter your second number: ");
+    io::stdin().read_line(&mut num2).expect("failed to read line");
+    println!();
+    let num2:f64 = num2.trim().parse().unwrap();
+    return num2;
+}
+
+fn op_checker(op: String, num1: f64, num2: f64) {
     if op == "+" {
-        println!("{}", num1 + num2);
+        println!("Result: {}", num1 + num2);
     }
 
     else if op == "-" {
-        println!("{}", num1 - num2);
+        println!("Result: {}", num1 - num2);
     }
 
     else if op == "*" {
-        println!("{}", num1 * num2);
+        println!("Result: {}", num1 * num2);
     }
 
     else if op == "/" {
-        println!("{}", num1 / num2);
+        println!("Result: {}", num1 / num2);
     }
 
     else {
         println!("Please enter a valid operator!");
+        println!();
+        let new_op = get_op();
+        op_checker(new_op, num1, num2);
+
+    }
+
+    println!();
+    let mut restart = String::new();
+    eprint!("Enter 'r' to restart or press enter to close: ");
+    io::stdin().read_line(&mut restart).expect("failed to read line");
+    let restart = restart.trim();
+
+    if restart == "r" {
+        println!();
+        println!();
+        main();
+        return;
+    }
+
+    else {
+        return;
     }
 
 }
